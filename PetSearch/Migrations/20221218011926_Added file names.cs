698264a1c6652f7a866e9446be108211b0333492
Лялин Артем Анатольевич <a.lyalin@skbkontur.ru>
2022-12-18 20:41:50 +0500

    Migration for file uploading

---
+++ PetSearch/Migrations/20221218011926_Added file names.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PetSearch.Migrations
+{
+    public partial class Addedfilenames : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FileNames",
+                table: "LostAnimals",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FileNames",
+                table: "LostAnimals");
+        }
+    }
+}
